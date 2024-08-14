850893b2d58649e3146ff4a03bdd67ab40cb08fb
sybillereuter <sybille.reuter.berlin@gmail.com>
2024-08-14 18:58:32 +0200

    nltk_data: load language-specific averaged perceptron tagger

--- clean_tags.py
+++ clean_tags.py
@@ -8,6 +8,7 @@ from nltk.stem import WordNetLemmatizer
 
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
+nltk.download('averaged_perceptron_tagger_eng')
 nltk.download('wordnet')
 
 lemmatizer = WordNetLemmatizer()
