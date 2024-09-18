cfe868290ec963d06ec4e94207edef1413256eb2
sybillereuter <sybille.reuter.berlin@gmail.com>
2024-09-18 14:46:18 +0200

    added regression test (for checks during development)

--- test_clip_analyzer.py
+++ test_clip_analyzer.py
@@ -9,13 +9,24 @@ class TestGenerateTags(unittest.TestCase):
         self.image = Image.open(image_path)
         self.number_of_tags = 30
         self.tags = clip_analyzer.generate(self.image, self.number_of_tags)
-        print(self.tags)
 
     def test_tags_not_empty(self):
         self.assertTrue(len(self.tags) > 0)
 
     def test_tags_contains_birds(self):
         self.assertIn("Birds", self.tags)
+
+    @unittest.skip("Not deterministic; test is just here to make sure quality is not massively worse after a change.")
+    def test_regression(self):
+        old_tags = ['Flora and Fauna', 'Birds', 'Nature and Well-Being', 'Cooperation',
+                    'Nature-Centric Living', 'Wildlife Photography', 'Nature Conservation',
+                    'Mindfulness in Relationships', 'Romance', 'Connection to Nature',
+                    'Belonging', 'Animals', 'Connection and Well-Being', 'Love as Inspiration',
+                    'Compassion as Inspiration', 'Nature', 'Interpersonal Relationships',
+                    'Conservation', 'Ecotherapy', 'Nature Photography', 'Animal Photography',
+                    'Biodiversity', 'Wildlife Conservation', 'Conflict Resolution', 'Wildlife',
+                    'Nature as Healer']
+        self.assertCountEqual(old_tags, self.tags)
 
 
 if __name__ == "__main__":
