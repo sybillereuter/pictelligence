0c176c8881dcec141bb0a788535a4edb56bbbbc8
sybillereuter <sybille.reuter.berlin@gmail.com>
2025-02-11 19:06:41 +0100

    v1.0.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='pictelligence',
-    version='1.0.1',
+    version='1.0.2',
     packages=find_packages(),
     install_requires=install_requires,
     package_data={'pictelligence': ['tags/*.txt', 'sample_photos/*']},
